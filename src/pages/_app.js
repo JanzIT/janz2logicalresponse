f4dd05dcdcbd884b4fcc320ce7f729d5831412b5
Renan Janz H Vieira <janzhv.it@gmail.com>
2024-07-11 22:47:45 +0100

    Implemented Mantine, UX improvements, added CSS, more translations

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,19 +1,28 @@
 import "@/styles/globals.css";
 import { Flip, ToastContainer } from "react-toastify";
-import 'react-toastify/dist/ReactToastify.css'
+import 'react-toastify/dist/ReactToastify.css';
 import { appWithTranslation } from 'next-i18next';
+import '@mantine/core/styles.css';
+import { createTheme, MantineProvider } from '@mantine/core';
 
- function App({ Component, pageProps }) {
-  return <>
-    <Component {...pageProps} />
-    <ToastContainer
-      newestOnTop
-      stacked
-      transition={Flip}
-      style={{ color: 'white' }}
-      pauseOnFocusLoss={false}
-    />
-  </>
+
+const theme = createTheme({
+ //
+});
+
+function App({ Component, pageProps }) {
+  return (
+    <MantineProvider theme={theme}>
+      <Component {...pageProps} />
+      <ToastContainer
+        newestOnTop
+        stacked
+        transition={Flip}
+        style={{ color: 'white' }}
+        pauseOnFocusLoss={false}
+      />
+    </MantineProvider>
+  );
 }
 
-export default appWithTranslation(App)
+export default appWithTranslation(App);
